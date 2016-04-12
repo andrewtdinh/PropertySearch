04d487e1b8200944f31cbf9835c72badb12bfba7
Andrew Dinh <andrewdinh74@yahoo.com>
2016-04-12 00:27:15 -0700

    Add styles to SearchPage

--- SearchPage.js
+++ SearchPage.js
@@ -11,3 +11,17 @@ var {
   Image,
   Component
 } = React;
+
+var styles = StyleSheet.create({
+  description: {
+    marginBottom: 20,
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565'
+  },
+  container: {
+    padding: 30,
+    marginTop: 65,
+    alignItems: 'center'
+  }
+});
