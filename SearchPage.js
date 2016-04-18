1f75a0baa2f7ced60af53863b8abf2738c58d9ea
Andrew Dinh <andrewdinh74@yahoo.com>
2016-04-17 21:38:28 -0700

    Update _executeQuery function

--- SearchPage.js
+++ SearchPage.js
@@ -96,6 +96,14 @@ class SearchPage extends Component {
   _executeQuery(query) {
     console.log(query);
     this.setState({ isLoading: true });
+    fetch(query)
+      .then(response => response.json())
+      .then(json => this._handleResponse(json.response))
+      .catch(error =>
+        this.setState({
+          isLoading: false,
+          message: 'Something bad happened ' + error
+        }));
   }
   onSearchPressed() {
     var query = urlForQueryAndPage('place_name', this.state.searchString, 1);
