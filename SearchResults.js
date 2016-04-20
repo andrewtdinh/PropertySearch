b4729dc302800577e75214c51f9c6ebf8ac9ebc7
Andrew Dinh <andrewdinh74@yahoo.com>
2016-04-20 18:03:00 -0400

    Update rowPressed method

--- SearchResults.js
+++ SearchResults.js
@@ -52,6 +52,12 @@ class SearchResults extends Component {
     }
   rowPressed(propertyGuid) {
     var property = this.props.listings.filter(prop => prop.guid === propertyGuid)[0];
+
+    this.props.navigator.push({
+      title: "Property",
+      component: PropertyView,
+      passProps: {property: property}
+    });
   }
   renderRow(rowData, sectionID, rowID) {
     var price = rowData.price_formatted.split(' ')[0];
